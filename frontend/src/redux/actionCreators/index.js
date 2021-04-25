bcf19762ed68c19bd614ada68fa022faafee4b75
Jaakko Rajala <jaakko.rajala@tuni.fi>
2021-04-25 21:11:02 +0300

    Connect OrderService.js to redux-store (action creators)  with thunk.

--- frontend/src/redux/actionCreators/index.js
+++ frontend/src/redux/actionCreators/index.js
@@ -1,4 +1,5 @@
 /** @format */
+import { createOrder, getOrder, getOrders } from '../../services/OrderService';
 import {
 	ADD_OLD_ORDERS,
 	ADD_ORDER,
@@ -12,31 +13,61 @@ import {
 } from '../constants';
 
 // ORDER ACTION CREATORS
+/**
+ *
+ * @param {Object} newOrder
+ * @returns
+ */
 export const addOrder = (newOrder) => {
-	return {
-		type: ADD_ORDER,
-		payload: newOrder,
+	// Add thunk here too
+	return async (dispatch) => {
+		// Server A not yet implemented
+		// newOrder = createOrder(newOrder)
+		dispatch({
+			type: ADD_ORDER,
+			payload: newOrder,
+		});
 	};
 };
 
+/**
+ * Action creator that dispatches the removal action of the given order to frontend redux store once order from the DB is removed permanently -- NOT IMPLEMENTED for backend!
+ * @param {} order
+ * @returns
+ */
 export const removeOrder = (order) => {
 	// Add thunk here (to remove from DB)
-	return {
-		type: REMOVE_ORDER,
-		payload: order,
+	return async (dispatch) => {
+		// No path for it in back end (or an HTTP-request function at OrderService.js), so only dispatch here.
+		dispatch({
+			type: REMOVE_ORDER,
+			payload: order,
+		});
 	};
 };
 
-export const addOldOrders = () => {
-	// Add thunk here.
-	return {
-		type: ADD_OLD_ORDERS,
-		payload: [],
+/**
+ * Action creator that dispatches all the orders it receives from DB to the frontends redux-stores order-state.
+ * @returns {Object} action
+ */
+export const fetchOrders = () => {
+	return async (dispatch) => {
+		let orders;
+		// Server A not yet implemented- uncomment when done.
+		// orders = getOrders();
+		dispatch({
+			type: ADD_OLD_ORDERS,
+			payload: orders,
+		});
 	};
 };
 
+/**
+ * TO update the state of the specificied order.
+ * @param {Object} order
+ * @returns {Object} action
+ */
 export const updateOrderState = (order) => {
-	// Add thunk here.
 	return {
 		type: UPDATE_ORDER_STATE,
 		payload: order,
@@ -44,12 +75,24 @@ export const updateOrderState = (order) => {
 };
 
 // CART ACTION CREATORS
+
+/**
+ * Action creator that dispatches an action to update the cart-state.
+ * @param {Object} newItem
+ * @returns
+ */
 export const addCartItem = (newItem) => {
 	return {
 		type: ADD_CART_ITEM,
 		payload: newItem,
 	};
 };
+
+/**
+ * Action creator that dispatches an action to remove an item from the cart-state.
+ * @param {*} item
+ * @returns
+ */
 export const removeCartItem = (item) => {
 	return {
 		type: REMOVE_CART_ITEM,
@@ -57,6 +100,12 @@ export const removeCartItem = (item) => {
 	};
 };
 
+/**
+ * Action creator that updates the amount of the given ID
+ * @param {Number} id
+ * @param {Number} amount
+ * @returns {Object}
+ */
 export const updateCartItem = (id, amount) => {
 	return {
 		type: UPDATE_CART_ITEM_AMOUNT,
@@ -64,6 +113,10 @@ export const updateCartItem = (id, amount) => {
 	};
 };
 
+/**
+ * An action creator which creates an action to remove cart items.
+ * @returns {Object}
+ */
 export const emptyCart = () => {
 	return {
 		type: EMPTY_CART,
@@ -71,6 +124,11 @@ export const emptyCart = () => {
 };
 
 // NOTIFICATION ACTION CREATORS
+/**
+ * An action creator which adds a new Notification to the notification-state.
+ * @param {*} newNotification
+ * @returns
+ */
 export const addNotification = (newNotification) => {
 	return {
 		type: NEW_NOTIFICATION,
--- frontend/src/services/OrderService.js
+++ frontend/src/services/OrderService.js
@@ -2,11 +2,31 @@
 import axios from 'axios';
 const orderURL = 'http://localhost:12345/v1/order';
 
+/**
+ * Sends an HTTP GET request with the help of axios to orderURL. Gets the order with the specified id.
+ * @param {String} id of the order
+ * @returns {Object}
+ */
 export const getOrder = async (id) => {
 	const response = await axios.get(orderURL + '/' + id);
 	return response.data;
 };
 
+/**
+ * * Sends an HTTP GET request with the help of axios to orderURL. Gets all orders.
+ * @param {*} id
+ * @returns
+ */
+export const getOrders = async (id) => {
+	const response = await axios.get(orderURL);
+	return response.data;
+};
+
+/**
+ * Sends an HTTP GET request with the help of axios to orderURL.
+ * @param {Object} order
+ * @returns
+ */
 export const createOrder = async (order) => {
 	const response = await axios.post(orderURL, order);
 	return response.data;
